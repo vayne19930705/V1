6a310091c5e7a57d1c667ac2bc106f9afbd6180f
juexi <yangjuexi@qq.com>
2018-07-20 00:21:22 +0800

    hello VSCode

---
+++ src/com/yangjuexi/first/HelloVSCode.java
@@ -0,0 +1,12 @@
+package com.yangjuexi.first;
+
+/**
+ * @author yangjuexi HelloVSCode
+ */
+public class HelloVSCode
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "HelloVSCode" );
+    }
+}
